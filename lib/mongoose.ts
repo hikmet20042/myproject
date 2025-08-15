4cc9625d6ae168c6ee22904bee82c7aa7aac3be8
Hikmat <memmedlih032@gmail.com>
2025-08-15 12:39:12 +0400

    fix

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,11 +1,6 @@
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
 
 let cached = (global as any).mongoose;
 if (!cached) {
@@ -13,6 +8,10 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  const MONGODB_URI = process.env.MONGODB_URI as string;
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable.');
+  }
   if (cached.conn) {
     return cached.conn;
   }
